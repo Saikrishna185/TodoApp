03295e067dddce1d212e16b5cb5d7d8b28431592
Saikrishna185 <saikrishnasahu143@gmail.com>
2026-01-03 22:35:36 +0530

    Add modified Controller, Service and basic html page of Simple TODO App

--- src/main/java/com/sk/app/todoapp/controller/TaskController.java
+++ src/main/java/com/sk/app/todoapp/controller/TaskController.java
@@ -5,8 +5,7 @@ import com.sk.app.todoapp.services.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,23 @@ public class TaskController {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+    
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/tasks";
+    }
+    
+    @PostMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/tasks";
+    }
+    
+    @PostMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/tasks";
     }
 }
